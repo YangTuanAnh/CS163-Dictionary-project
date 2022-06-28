8ac52604f2f63e05b6dcb3812b44fe424820b7e5
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2022-06-28 15:15:15 +0700

    - Added 2 classes in the file Trie.h and Trie.cpp:    + Trie_Node and Trie_Node() constructor    + Trie, Trie() constructor and Trie::trie_search() method

---
+++ src/utility/Trie.cpp
@@ -0,0 +1,47 @@
+#include "Trie.h"
+
+int alphabetic_branch(char x)
+// Returns the branch corresponding to char x
+{
+	// ASCII used
+	// Currently for English alphabet
+	if (x >= 97 && x <= 122) // Lowercase
+		return x - 97;
+	else if (x >= 65 && x <= 90) // Uppercase
+		return x - 65;
+	return -1; // Not a letter
+}
+/*******************************************************************************************************/
+// Trie_Node Defs
+Trie_Node::Trie_Node()
+{
+	for (int i = 0; i < MAX_BRANCH; i++)
+		branch[i] = nullptr;
+}
+/*******************************************************************************************************/
+// Trie Defs
+
+template <class Record>
+Trie<Record>::Trie()
+{
+	root = nullptr;
+}
+
+template <class Record>
+Error_Code Trie<Record>::trie_search(Record target, Record& result)
+{
+	int position = 0;
+	Trie_Node<Record>* cur_Node = root;
+	while (cur_Node && position < target.data.length())
+	{
+		cur_Node = cur_Node->branch[alphabetic_branch(target.data[position])];
+		position++;
+	}
+	if (cur_Node)
+	{
+		result = cur_Node->data;
+		return success;
+	}
+	else
+		return non_exist;
+}
---
+++ src/utility/Trie.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "search.h"
+
+const int MAX_BRANCH = 28;
+
+enum Error_Code{success, non_exist};
+
+template <class Record> class Trie_Node;
+template <class Record> class Trie;
+
+int alphabetic_branch(char x); // Returns the alphabetic order of x
+
+template <class Record>
+class Trie_Node
+{
+public:
+	// data members
+	Record* data;
+	Trie_Node<Record>* branch[MAX_BRANCH];
+	// Constructors
+	Trie_Node();
+};
+
+template <class Record>
+class Trie
+{
+public:
+	// Constructor
+	Trie();
+	// Methods
+	Error_Code trie_search(Record target, Record& result);
+private:
+	Trie_Node<Record>* root;
+	
+};
