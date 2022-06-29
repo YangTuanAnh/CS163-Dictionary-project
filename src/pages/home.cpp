24c0888369fe180e800e469f9417dcbae52c48be
YangTuanAnh <yangtuananh2003@gmail.com>
2022-06-29 15:59:59 +0700

    Add Definition Panel

--- src/pages/home.cpp
+++ src/pages/home.cpp
@@ -22,61 +22,78 @@ void Home::update()
     }
     if (SearchEdit)
     {
-        if (GetKeyPressed())
+        Vector2 mousePos = GetMousePosition();
+        for (int i = 0; i < word.size(); i++)
         {
-            for (int i = 0; i < 20; i++)
-                rec_result[i] = {350, (float)200 + 120 * i, 825, 115};
+            if (CheckCollisionPointRec(mousePos, rec_result[i]) && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+                selectedWord = word[i];
         }
     }
 }
 
 void Home::draw()
 {
-    DrawRectangleRec(rec_modes, WHITE);
+    LoadDefinition(selectedWord);
 
+    DrawRectangleRec(rec_modes, WHITE);
     Vector2 mousePos = GetMousePosition();
-    if (rec_modes.x <= mousePos.x && mousePos.x <= rec_modes.x + rec_modes.width &&
-        rec_modes.y <= mousePos.y && mousePos.y <= rec_modes.y + rec_modes.height)
-    {
-        int index = (mousePos.y - rec_modes.y) / (rec_modes.height / Modes.size());
-        if (index < Modes.size())
-        {
-            DrawRectangle(rec_modes.x, rec_modes.y + index * (rec_modes.height / Modes.size()), rec_modes.width, rec_modes.height / Modes.size(), LIGHTGRAY);
-            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            {
-                DrawRectangle(rec_modes.x, rec_modes.y + index * (rec_modes.height / Modes.size()), rec_modes.width, rec_modes.height / Modes.size(), GRAY);
-                std::cerr << "Load " << Modes[index] << '\n';
-            }
-        }
-    }
+
     for (int i = 0; i < Modes.size(); i++)
     {
+        Rectangle rec_mode = {rec_modes.x, rec_modes.y + i * (rec_modes.height / Modes.size()), rec_modes.width, rec_modes.height / Modes.size()};
+        if (CheckCollisionPointRec(mousePos, rec_mode))
+        {
+            DrawRectangleRec(rec_mode, LIGHTGRAY);
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+            {
+                DrawRectangleRec(rec_mode, GRAY);
+                std::cerr << "Load " << Modes[i] << '\n';
+            }
+        }
         DrawText(Modes[i].c_str(), rec_modes.x + 20, rec_modes.y + rec_modes.height * (i + 0.5) / Modes.size(), 20, BLACK);
     }
     DrawRectangleLinesEx(rec_modes, 3, BLACK);
 
-    if (SearchInput[0] != '\0')
+    DrawRectangleLinesEx(rec_search, 3, BLACK);
+    if (GuiTextBox(rec_search, SearchInput, 20, SearchEdit))
+        SearchEdit ^= 1;
+
+    if (SearchEdit)
     {
         for (int i = 0; i < word.size(); i++)
         {
             DrawRectangleRec(rec_result[i], DARKBLUE);
+            if (CheckCollisionPointRec(mousePos, rec_result[i]))
+                DrawRectangleRec(rec_result[i], BLUE);
+
             DrawTextEx(fnt, word[i]->data.c_str(), {rec_result[i].x + 13, rec_result[i].y + 10}, 25, 2, WHITE);
             for (int j = 0; j < word[i]->defs.size(); j++)
                 DrawTextEx(fnt, word[i]->defs[j]->data.c_str(), {rec_result[i].x + 13, rec_result[i].y + 40 * (j + 1)}, 25, 2, WHITE);
         }
-    }
-    DrawRectangle(350, 100, 850, 90, RAYWHITE);
-    DrawRectangleLinesEx(rec_search, 3, BLACK);
-    if (GuiTextBox(rec_search, SearchInput, 20, SearchEdit))
-    {
-        for (int i = 0; i < 20; i++)
-            rec_result[i] = {350, (float)200 + 120 * i, 825, 115};
-        SearchEdit ^= 1;
     }
 
     if (GuiButton(rec_search_button, "Search"))
         std::cerr << "Pressed search button\n";
 
     if (SearchInput[0] == '\0')
-        DrawText("Search bar", 385, 135, 30, LIGHTGRAY);
+        DrawText("Search bar", 375, 135, 30, LIGHTGRAY);
+}
+
+void Home::LoadDefinition(Word *word = NULL)
+{
+    if (!word)
+        return;
+    DrawRectangleRec(rec_def, WHITE);
+    DrawRectangleLinesEx(rec_def, 3, BLACK);
+
+    const int button_width = 45;
+    if (GuiButton({rec_def.x + 15, rec_def.y + 15, button_width, button_width}, "->"))
+        selectedWord = NULL;
+    GuiButton({rec_def.x + rec_def.width - 15 - button_width, rec_def.y + 15, button_width, button_width}, "X");
+    GuiButton({rec_def.x + rec_def.width - (15 + button_width) * 2, rec_def.y + 15, button_width, button_width}, "<3");
+    GuiButton({rec_def.x + rec_def.width - (15 + button_width) * 3, rec_def.y + 15, button_width, button_width}, "!!");
+
+    DrawTextEx(fnt, word->data.c_str(), {rec_def.x + 15, rec_def.y + 30 + button_width}, 40, 2, BLACK);
+    for (int j = 0; j < word->defs.size(); j++)
+        DrawTextEx(fnt, word->defs[j]->data.c_str(), {rec_def.x + 15, rec_def.y + 50 + button_width + 40 * (j + 1)}, 25, 2, BLACK);
 }
--- src/pages/home.h
+++ src/pages/home.h
@@ -9,15 +9,22 @@ private:
     bool SearchEdit = false;
     char SearchInput[101] = "";
     std::vector<Word *> word;
+    Word *selectedWord = NULL;
     Rectangle rec_result[20];
-    Rectangle rec_modes{25, 115, 310, 470};
+    Rectangle rec_modes{25, 115, 310, 475};
     Rectangle rec_search{350, 115, 620, 65};
     Rectangle rec_search_button{1000, 115, 175, 65};
+    Rectangle rec_def{350, 200, 825, 390};
     Font fnt = LoadFont("../data/Font.ttf");
     std::vector<std::string> Modes = {"Eng-Eng", "Eng-Vie", "Slang", "Emotional", "ABC", "CDE"};
 
 public:
-    void
-    update();
+    Home()
+    {
+        for (int i = 0; i < 20; i++)
+            rec_result[i] = {350, (float)200 + 120 * i, 825, 115};
+    }
+    void update();
     void draw();
+    void LoadDefinition(Word *word);
 };
