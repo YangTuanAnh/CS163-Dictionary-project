d13946ec2a33f047dab496171b3395127062218f
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-02 12:03:08 +0700

    Fix text overflow

--- src/pages/home.cpp
+++ src/pages/home.cpp
@@ -23,11 +23,11 @@ void Home::update()
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && GetMousePosition().y > 180 && CheckCollisionPointRec(GetMousePosition(), rec_result[i]))
         {
             selectedWord = word[i];
-            
+
             slang.getFullDefinition(selectedWord->data);
 
             for (int i = 0; i < 20; i++)
-                rec_result[i] = { 350, (float)200 + 120 * i, 800, 115 };
+                rec_result[i] = {350, (float)200 + 120 * i, 800, 115};
         }
     }
     if (SearchEdit)
@@ -35,7 +35,7 @@ void Home::update()
         if (GetKeyPressed())
         {
             for (int i = 0; i < 20; i++)
-                rec_result[i] = { 350, (float)200 + 120 * i, 800, 115 };
+                rec_result[i] = {350, (float)200 + 120 * i, 800, 115};
         }
     }
 }
@@ -47,7 +47,7 @@ void Home::draw()
 
     for (int i = 0; i < Modes.size(); i++)
     {
-        Rectangle rec_mode = { rec_modes.x, rec_modes.y + i * (rec_modes.height / Modes.size()), rec_modes.width, rec_modes.height / Modes.size() };
+        Rectangle rec_mode = {rec_modes.x, rec_modes.y + i * (rec_modes.height / Modes.size()), rec_modes.width, rec_modes.height / Modes.size()};
         if (CheckCollisionPointRec(mousePos, rec_mode))
         {
             DrawRectangleRec(rec_mode, LIGHTGRAY);
@@ -57,8 +57,9 @@ void Home::draw()
                 std::cerr << "Load " << Modes[i] << '\n';
             }
         }
-        if (modeChosen == i) DrawRectangleRec(rec_mode, GRAY);
-        DrawTextEx(fnt, Modes[i].c_str(), { rec_modes.x + 70, float(rec_modes.y + rec_modes.height * (i + 0.35) / Modes.size()) }, 35, 2, BLACK);
+        if (modeChosen == i)
+            DrawRectangleRec(rec_mode, GRAY);
+        DrawTextEx(fnt, Modes[i].c_str(), {rec_modes.x + 70, float(rec_modes.y + rec_modes.height * (i + 0.35) / Modes.size())}, 35, 2, BLACK);
     }
 
     DrawRectangleLinesEx(rec_modes, 3, BLACK);
@@ -70,16 +71,25 @@ void Home::draw()
         if (CheckCollisionPointRec(mousePos, rec_result[i]) && mousePos.y > 180)
             DrawRectangleRec(rec_result[i], BLUE);
 
-        DrawTextEx(fnt, word[i]->data.c_str(), { rec_result[i].x + 13, rec_result[i].y + 10 }, 25, 2, WHITE);
-        for (int j = 0; j < word[i]->defs.size(); j++)
-            DrawTextEx(fnt, word[i]->defs[j]->data.c_str(), { rec_result[i].x + 13, rec_result[i].y + 40 * (j + 1) }, 25, 2, WHITE);
+        DrawTextEx(fnt, word[i]->data.c_str(), {rec_result[i].x + 13, rec_result[i].y + 10}, 25, 2, WHITE);
+        for (int j = 0; j <= 1; j++)
+        {
+            std::string s = word[i]->defs[j]->data;
+            if (s.length() * 12 > rec_result[i].width - 13)
+            {
+                for (int k = 1; k <= 3; k++)
+                    s.insert(s.begin() + rec_result[i].width / 12 + k, '.');
+                s.insert(s.begin() + rec_result[i].width / 12 + 4, '\0');
+            }
+            DrawTextEx(fnt, s.c_str(), {rec_result[i].x + 13, rec_result[i].y + 40 * (j + 1)}, 25, 2, WHITE);
+        }
     }
     DrawRectangle(350, 100, 850, 90, RAYWHITE);
     DrawRectangleLinesEx(rec_search, 3, BLACK);
     if (GuiTextBox(rec_search, SearchInput, 20, SearchEdit))
     {
         for (int i = 0; i < 20; i++)
-            rec_result[i] = { 350, (float)200 + 120 * i, 800, 115 };
+            rec_result[i] = {350, (float)200 + 120 * i, 800, 115};
         SearchEdit ^= 1;
     }
 
@@ -92,7 +102,7 @@ void Home::draw()
     }
 }
 
-bool Home::LoadDefinition(Word* word = NULL)
+bool Home::LoadDefinition(Word *word = NULL)
 {
     if (!word)
     {
@@ -105,16 +115,34 @@ bool Home::LoadDefinition(Word* word = NULL)
 
     const int button_width = 100;
 
-    GuiButton({ rec_def.x + rec_def.width - 15 - button_width, rec_def.y + rec_def.height - 60, button_width, 45 }, "Delete");
-    if (GuiButton({ rec_def.x + rec_def.width - (15 + button_width) * 3, rec_def.y + rec_def.height - 60, button_width * 2 + 15, 45 }, "Add Favorite"))
+    GuiButton({rec_def.x + rec_def.width - 15 - button_width, rec_def.y + rec_def.height - 60, button_width, 45}, "Delete");
+    if (GuiButton({rec_def.x + rec_def.width - (15 + button_width) * 3, rec_def.y + rec_def.height - 60, button_width * 2 + 15, 45}, "Add Favorite"))
     {
         insertFavorites(selectedWord);
         printFavorites();
     }
-    GuiButton({ rec_def.x + rec_def.width - (15 + button_width) * 4, rec_def.y + rec_def.height - 60, button_width, 45 }, "Edit");
+    GuiButton({rec_def.x + rec_def.width - (15 + button_width) * 4, rec_def.y + rec_def.height - 60, button_width, 45}, "Edit");
 
-    DrawTextEx(fnt, word->data.c_str(), { rec_def.x + 15, rec_def.y + 30 + button_width }, 40, 2, BLACK);
+    DrawTextEx(fnt, word->data.c_str(), {rec_def.x + 15, rec_def.y + 40}, 40, 2, BLACK);
+    int cnt = 1;
     for (int j = 0; j < word->defs.size(); j++)
-        DrawTextEx(fnt, word->defs[j]->data.c_str(), { rec_def.x + 15, rec_def.y + 50 + button_width + 40 * (j + 1) }, 25, 2, BLACK);
+    {
+        std::string s = word->defs[j]->data;
+        int extended = 1;
+        if (s.length() * 10 > rec_def.width - 15)
+        {
+            int len = (rec_def.width - 15) / 10, temp = len;
+            while (temp < s.length())
+            {
+                while (s[temp] != ' ' && temp < s.length())
+                    temp--;
+                s.insert(s.begin() + temp, '\n');
+                temp += len;
+                extended++;
+            }
+        }
+        DrawTextEx(fnt, s.c_str(), {rec_def.x + 15, rec_def.y + 60 + 30 * cnt}, 20, 2, BLACK);
+        cnt += extended;
+    }
     return true;
 }
--- src/utility/Dictionary.h
+++ src/utility/Dictionary.h
@@ -15,40 +15,40 @@ class Word
 {
 public:
     std::string data;
-    std::vector<Definition*> defs;
-    Word(const std::string& s);
+    std::vector<Definition *> defs;
+    Word(const std::string &s);
 };
 
 class Definition
 {
 public:
     std::string data;
-    Word* word;
-    Definition(const std::string& s);
+    Word *word;
+    Definition(const std::string &s);
 };
 
-class Dictionary 
+class Dictionary
 {
 public:
-    Dictionary(const std::string& _dir, const std::string& chars);
+    Dictionary(const std::string &_dir, const std::string &chars);
     ~Dictionary();
-    std::vector<Word*> SearchWord(const std::string& word);
-    std::vector<Word*> SearchDef(const std::string& key);
-    std::vector<std::string> getFullDefinition(const std::string& word);
+    std::vector<Word *> SearchWord(const std::string &word);
+    std::vector<Word *> SearchDef(const std::string &key);
+    std::vector<std::string> getFullDefinition(const std::string &word);
 
 private:
     std::string dir;
-    std::vector<Definition*> allDefs;
-    std::vector<Word*> history;
-    Trie<Word*>* trie;
+    std::vector<Definition *> allDefs;
+    std::vector<Word *> history;
+    Trie<Word *> *trie;
 
     void LoadData();
     void LoadHistory();
-    void updateHistory(Word* word);
-    std::vector<Word*> getSearchHistory();
+    void updateHistory(Word *word);
+    std::vector<Word *> getSearchHistory();
     void saveData();
     void saveHistory();
 };
 
-std::vector<std::string> Split(const std::string& s, char delim);
-bool IsPrefix(const std::string& p, const std::string& s);
+std::vector<std::string> Split(const std::string &s, char delim);
+bool IsPrefix(const std::string &p, const std::string &s);
