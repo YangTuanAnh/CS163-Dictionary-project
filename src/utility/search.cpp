50fbf3c1198086f17dc23d1ac7dbf2825377c6da
NgHuuTrong <trongnh2003@gmail.com>
2022-07-23 16:32:56 +0700

    Update search.cpp

--- src/utility/search.cpp
+++ src/utility/search.cpp
@@ -4,4 +4,4 @@
 //Dictionary slang("CS163_github/data/slang", uppercase + symbols + digits + space);
 
 //Dictionary engeng("CS163_github/data/engeng", lowercase + digits + space + "-\'.");
-Dictionary slang("CS163_github/data/engeng",lowercase + digits + space + "-\'.");
+Dictionary slang("../data/engeng",lowercase + digits + space + "-\'.");
